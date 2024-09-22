728feb62d0934f889d6b5f94d59a4ccf53e040d7
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-09-22 09:25:53 +0600

    constructor and factory

---
+++ constructorVSfactory.js
@@ -0,0 +1,6 @@
+// Constructor Pattern
+// Constructor pattern is a way to declare array as like declare a object with constructor same way.
+// we can declare an array with constructor pattern with new keyword. let's see the example
+
+
+const arr = Array()/* this is the way to declare an array with constructor, we need a variable to store this new array. */
