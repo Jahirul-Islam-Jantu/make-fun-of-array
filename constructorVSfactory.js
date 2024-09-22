73f10521a638c61151ec6bda03ab91d983a339ed
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-09-22 11:49:51 +0600

    array travarsing

--- constructorVSfactory.js
+++ constructorVSfactory.js
@@ -3,4 +3,24 @@
 // we can declare an array with constructor pattern with new keyword. let's see the example
 
 
-const arr = Array()/* this is the way to declare an array with constructor, we need a variable to store this new array. */
+const arr = Array(5)/* this is the way to declare an array with constructor, we need a variable to store this new array. */
+arr.push(["Jahir", "John", "Jan"])
+arr[0] = "Jahir"
+arr[1] = "Jan"
+arr[2] = "Feb"
+console.log(arr)
+
+// Factory Pattern
+
+const arr2 = Array()
+console.log(arr2, arr2.length)
+
+const arr3 = Array(5)
+console.log(arr3, arr3.length)
+
+const arr4 = Array(5)
+arr4.push([1, 2, 3])
+arr4[0] = "Jan"
+arr4[1] = "Feb"
+arr4[2] = "Mar"
+console.log(arr4)
---
+++ travarseArray.js
@@ -0,0 +1,31 @@
+// Array Travarsing
+/**
+ * Array travarsing is like attendance sheet of a school, we have a student's attendance sheet and we call everyones id everytime. this is array travarsing. travarse on every single item of an array.
+ * */
+/*
+// simple travers
+    const arr1 = [1, 2, 3, 4, 5];
+     for (let i = 0; i < arr1.length; i++) {
+         console.log(arr1[i]);
+     }
+ */
+/*
+
+// simple array travarse with sum every item
+const arr = [1, 2, 3, 4, 5, 6];
+let count = 0;
+for (let i = 0; i < arr.length; i++) {
+    count += arr[i];
+    console.log(count, "Count");
+}
+ */
+/*
+// sum and average of an array
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+let sum = 0;
+for (let i = 0; i < array.length; i++) {
+    sum += array[i]
+}
+console.log(sum, "sum")
+
+ */
